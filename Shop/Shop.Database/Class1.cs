9e6412603796c2935d0875d088d52e27e0413e3a
steniojoseph72 <steniojoseph@yahoo.com>
2021-08-09 11:01:55 -0400

    Set the project structure, added Shop.Application, Shop.Domain and Shop.Database class Library Project

---
+++ Shop/Shop.Database/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shop.Database
+{
+    public class Class1
+    {
+    }
+}
---
+++ Shop/Shop.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shop.Domain
+{
+    public class Class1
+    {
+    }
+}
